803d126776ca7d2a83a4f5783eb344049fd53e4a
Brandon Him <bhim@anthropologie.com>
2015-07-18 15:42:45 -0400

    Update 2015-07-18T19:42:44.840Z

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('nclreport', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ngRoute', 'firebase', 'chartjs-directive']);
-
-})();
